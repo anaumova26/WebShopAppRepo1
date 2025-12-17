575fc6e139a7a2339c8f011f883b33fe62837675
anaumova26 <a.naumova2021@pmg-pernik.com>
2025-12-17 12:04:04 +0200

    part5

--- WebShopAppp/WebShopAppp/Program.cs
+++ WebShopAppp/WebShopAppp/Program.cs
@@ -36,7 +36,7 @@ namespace WebShopAppp
             builder.Services.AddTransient<ICategoryService, CategoryService>();
             builder.Services.AddTransient<IBrandService, BrandService>();
             builder.Services.AddTransient<IProductService, ProductService>();
-
+            builder.Services.AddTransient<IOrderService, OrderService>();
             var app = builder.Build();
             app.PrepareDatabase();
 
